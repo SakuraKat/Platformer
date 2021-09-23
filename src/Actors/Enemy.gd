9cfbd4971db118eb4c41e1636c4f6961930c3b1e
lolzkatXD <91024758+SakuraKat@users.noreply.github.com>
2021-09-23 22:00:08 +0530

    video part 1

---
+++ src/Actors/Enemy.gd
@@ -0,0 +1,20 @@
+extends Actors
+
+func _ready() -> void:
+	set_physics_process(false)
+	_velocity.x = -speed.x
+
+func _on_StompDetector_body_entered(body: PhysicsBody2D) -> void:
+	if body.global_position.y > get_node("StompDetector").global_position.y:
+		print(body.global_position.y, " ",  get_node("StompDetector").global_position.y, " ", body.global_position.y > get_node("StompDetector").global_position.y)
+		return
+	
+	queue_free()
+
+func _physics_process(delta: float) -> void:
+	_velocity.y += gravity * delta
+	
+	if is_on_wall():
+		_velocity.x *= -1.0
+	
+	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
