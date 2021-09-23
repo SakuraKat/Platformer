09d76a6e0eded1d9a9a5d226f63287e07926f031
lolzkatXD <91024758+SakuraKat@users.noreply.github.com>
2021-09-23 22:03:30 +0530

    started from clean slate to focus on level design

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -1,26 +1,33 @@
-extends Actors
+extends Actor
 
-export var stomp_impulse = 1000
 
-func _on_EnemyDetector_area_entered(_area: Area2D) -> void:
+export var stomp_impulse: = 600.0
+
+
+func _on_StompDetector_area_entered(area: Area2D) -> void:
 	_velocity = calculate_stomp_velocity(_velocity, stomp_impulse)
 
-func _on_EnemyDetector_body_entered(body: Node) -> void:
-	queue_free()
 
-func _physics_process(_delta: float) -> void:
+func _on_EnemyDetector_body_entered(body: PhysicsBody2D) -> void:
+	die()
+
+
+func _physics_process(delta: float) -> void:
 	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
-	var direction = get_direction()
-	
+	var direction: = get_direction()
 	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
-	
-	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
+	var snap: Vector2 = Vector2.DOWN * 60.0 if direction.y == 0.0 else Vector2.ZERO
+	_velocity = move_and_slide_with_snap(
+		_velocity, snap, FLOOR_NORMAL, true
+	)
+
 
 func get_direction() -> Vector2:
 	return Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 0.0
+		-Input.get_action_strength("jump") if is_on_floor() and Input.is_action_just_pressed("jump") else 0.0
 	)
+
 
 func calculate_move_velocity(
 		linear_velocity: Vector2,
@@ -28,26 +35,19 @@ func calculate_move_velocity(
 		speed: Vector2,
 		is_jump_interrupted: bool
 	) -> Vector2:
-	var out: = linear_velocity
-	
-	out.x = speed.x * direction.x
-	
-	out.y += gravity * get_physics_process_delta_time()
-	if direction.y == -1.0:
-		out.y = speed.y * direction.y
+	var velocity: = linear_velocity
+	velocity.x = speed.x * direction.x
+	if direction.y != 0.0:
+		velocity.y = speed.y * direction.y
 	if is_jump_interrupted:
-		out.y = 0.0
-	
-	return out
-
-func calculate_stomp_velocity(
-		linear_velocity: Vector2,
-		impulse: float
-	) -> Vector2:
-		var out: = linear_velocity
-		
-		out.y = -impulse
-		
-		return out
+		velocity.y = 0.0
+	return velocity
 
 
+func calculate_stomp_velocity(linear_velocity: Vector2, stomp_impulse: float) -> Vector2:
+	var stomp_jump: = -speed.y if Input.is_action_pressed("jump") else -stomp_impulse
+	return Vector2(linear_velocity.x, stomp_jump)
+
+
+func die() -> void:
+	queue_free()
